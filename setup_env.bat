e00fa30562e51b2f711574d8407960311024e742
umang.gupta <umang.gupta@milliman.com>
2019-01-18 13:15:26 -0500

    Promotion Setup
    Add functionality promote versions of nyhealth-cms-quality-measures-detail

--- setup_env.bat
+++ setup_env.bat
@@ -8,20 +8,20 @@ rem   <none>
 
 rem ### LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
 
-echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Setting up nyhealth-cms-quality-measures env
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Setting up nyhealth-cms-quality-measures-detail env
 echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Running from %~f0
 
 echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Calling last promoted pipeline_components_env.bat
 call "S:\PRM\Pipeline_Components_Env\pipeline_components_env.bat"
 
 echo.
-echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Redirecting NYHEALTH_CMS_QUALITY_MEASURES_HOME to local copy
-SET NYHEALTH_CMS_QUALITY_MEASURES_HOME=%~dp0%
-echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: NYHEALTH_CMS_QUALITY_MEASURES is now %NYHEALTH_CMS_QUALITY_MEASURES_HOME%
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Redirecting NYHEALTH_CMS_QUALITY_MEASURES_DETAIL_HOME to local copy
+SET NYHEALTH_CMS_QUALITY_MEASURES_DETAIL_HOME=%~dp0%
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: NYHEALTH_CMS_QUALITY_MEASURES_DETAIL_HOME is now %NYHEALTH_CMS_QUALITY_MEASURES_DETAIL_HOME%
 
 echo.
 echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Prepending local copy of python library to PYTHONPATH
-set PYTHONPATH=%NYHEALTH_CMS_QUALITY_MEASURES_HOME%python;%PYTHONPATH%
+set PYTHONPATH=%NYHEALTH_CMS_QUALITY_MEASURES_DETAIL_HOME%python;%PYTHONPATH%
 echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: PYTHONPATH is now %PYTHONPATH%
 
-echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Finished setting up nyhealth-cms-quality-measures env
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Finished setting up nyhealth-cms-quality-measures-detail env
